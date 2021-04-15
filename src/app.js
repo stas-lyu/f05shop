e4e9e3ecb16a1cd57cdc1935a65731a1fe60c618
stas-lyu <staslyu1997@gmail.com>
2021-04-15 20:15:19 +0300

    add

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+require('slick-carousel');
 import './js/main.js';
 import './scss/base.scss';
 import './scss/typography.scss';
---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+import '../node_modules/materialize-css/dist/css/materialize.min.css';
+import '../node_modules/materialize-css/dist/js/materialize.min';
+
+$(document).ready(function(){
+    $('.tabs').tabs();
+});
