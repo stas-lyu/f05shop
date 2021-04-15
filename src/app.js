1bde4e192e2d1fda5fb0ab278b5ca6799a55a761
stas-lyu <staslyu1997@gmail.com>
2021-04-15 21:11:27 +0300

    add json

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 require('slick-carousel');
 import './js/main.js';
+import './js/test.js';
 import './scss/base.scss';
 import './scss/typography.scss';
--- src/js/main.js
+++
@@ -1,6 +0,0 @@
-import '../node_modules/materialize-css/dist/css/materialize.min.css';
-import '../node_modules/materialize-css/dist/js/materialize.min';
-
-$(document).ready(function(){
-    $('.tabs').tabs();
-});
